f1be0ec863591bda1baf14c709d72fea55922c19
Siddhartha Das <sd@SD15>
2020-08-14 11:16:05 +0530

    Final Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express=require("express"),
+request=require("request");
+var app=express();
+app.set("view engine","ejs");
+app.get("/results",function(req,res)
+{
+    var query=req.query.search;
+    var url='http://www.omdbapi.com/?s='+ query +'&apikey=thewdb';
+    request(url, function (error, response, body) {
+  if(!error && response.statusCode==200)
+  {
+      var data=JSON.parse(body);
+      res.render("result",{data:data});
+     
+  }
+});
+});
+app.get("/",function(req,res)
+{
+   res.render("search"); 
+});
+
+
+
+app.listen(process.env.PORT,process.env.IP,function()
+{
+    console.log("Server Started");
+});
